2af428ac37f3fba279b6b09233e92c3284afdb62
Jaime Vicencio <jai.vicencio@duocuc.cl>
2023-06-12 04:01:27 -0400

    prueba caos

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path
-from .views import (HomeView, ItemDetailView, ProductsView)
+from .views import (HomeView,
+                    CheckoutView,
+                    ItemDetailView,
+                    OrderSummaryView,
+                    ProductsView,
+                    add_to_cart,
+                    remove_from_cart,
+                    remove_single_item_from_cart,)
 
 app_name = 'core'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='core-home'),
+    path('checkout/', CheckoutView.as_view(), name='core-checkout'),
     path("products/",ProductsView.as_view(), name="core-products"),
-    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
+    path('product/<slug>/', ItemDetailView.as_view(), name='core-product'),
+    path('order-summary/', OrderSummaryView.as_view(), name='core-order-summary'),
+    path('add-to-cart/<slug>/', add_to_cart, name='core-add-to-cart'),
+    path('remove-from-cart/<slug>/', remove_from_cart, name='core-remove-from-cart'),
+    path('remove-item-from-cart/<slug>/', remove_single_item_from_cart,
+         name='core-remove-single-item-from-cart'),
 ]
--- jardineando_projecto/settings.py
+++ jardineando_projecto/settings.py
@@ -161,3 +161,9 @@ LOCALE_PATHS = [
 MEDIA_URL = 'core/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'core/media')
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "jardineandoweb"
+EMAIL_HOST_PASSWORD = "pzmimksdzlcqfdwp"
